be08e8924608b3e9b86510fd7c89231391dffcca
Thiago Zago <141664034+ThiagoZago@users.noreply.github.com>
2025-07-11 18:15:03 -0300

    Revert "Updated Card.js"
    This reverts commit 0e1d90a3f1d40e9be5ea3af19444dde8352a451c.

--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -1,14 +1,18 @@
-function Card({ children, className = '', animated = false, ...props }) {
-  return (
-    <div 
-      className={`card ${animated ? 'fade-in' : ''} ${className}`}
-      {...props}
-    >
-      <div className="card-body">
-        {children}
-      </div>
-    </div>
-  );
-}
-
-export default Card;
+@@ .. @@
+-import styles from './Card.module.css';
+-
+-function Card({ children, className = '', animated = false, ...props }) {
++function Card({ children, className = '', animated = false, ...props }) {
+   return (
+     <div 
+-      className={`${styles.card} ${animated ? styles.animated : ''} ${className}`}
++      className={`card ${animated ? 'fade-in' : ''} ${className}`}
+       {...props}
+     >
+-      {children}
++      <div className="card-body">
++        {children}
++      </div>
+     </div>
+   );
+ }
