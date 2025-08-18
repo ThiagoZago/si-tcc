dab5bead828975a78bcb52d5e6c9e8f0d58e2446
Thiago Zago <141664034+ThiagoZago@users.noreply.github.com>
2025-08-18 01:11:44 -0300

    basic update

--- react-project/src/components/layout/RightContent/RightContent.js
+++ react-project/src/components/layout/RightContent/RightContent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import BtnComponent from '../../BtnComponent';
 import styles from './RightContent.module.css';
 
